6bb706b5a2c888db6d7eaa27acdb667f86b98dd0
Ke Xu <kdotxu@gmail.com>
2016-09-28 11:10:47 +0800

    update readme

---
+++ README.md
@@ -0,0 +1,18 @@
+# snake-clj
+
+A snake game written in Clojure.
+
+### How to run
+
+##### Using Leiningen:
+
+```bash
+$ git clone git@github.com:markx/snake-clj.git
+$ cd snake-clj
+$ lein run
+```
+
+## License
+
+Distributed under the Eclipse Public License either version 1.0 or (at
+your option) any later version.
--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject snake-clj "0.1.0-SNAPSHOT"
   :description "A snake game written in Clojure"
-  :url "http://example.com/FIXME"
+  :url "https://github.com/markx/snake-clj"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
